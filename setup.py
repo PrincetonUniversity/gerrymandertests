55b7c1057d5eb977092779b2f230276642ff2c3c
Will Adler <will@wtadler.com>
2018-08-21 01:04:01 -0400

    better setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,39 @@
-from setuptools import setup
+long_description = """\
+Code for running 16 different metrics for measuring partisan gerrymandering, including:
 
-setup(
+- Mean-median difference and variant:
+   - Mean-median difference
+   - Equal vote weight
+- Lopsided margins (two-sample _t_-test on win margins)
+- Bootstrap (Monte Carlo) simulation
+- Declination variants
+   - Declination
+   - Declination (buffered)
+   - Declination variant
+   - Declination variant (buffered)
+- Efficiency gap variants
+   - Efficiency gap
+   - Difference gap
+   - Loss gap
+   - Surplus gap
+   - Vote-centric gap
+   - Vote-centric gap 2
+   - Tau gap
+- Partisan bias
+"""
+
+
+
+import setuptools
+
+setuptools.setup(
     name='gerrymetrics',
-    version='1.0dev',
+    version='0.1.1',
     packages=setuptools.find_packages(),
     description='Lots of metrics for quantifying gerrymandering',
     author='Princeton Gerrymandering Project',
     author_email='wtadler@princeton.edu',
     license='GNU General Public License v3.0',
-    long_description=open('README.md').read(),
+    long_description=long_description,
     install_requires=['pandas', 'numpy', 'scipy', 'matplotlib', 'tqdm']
 )
