a53ae5a862c277b8cdd56008755ceefd53946681
Will Adler <will@wtadler.com>
2019-01-14 18:48:51 -0500

    version update

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ import setuptools
 
 setuptools.setup(
     name='gerrymetrics',
-    version='0.1.2',
+    version='0.1.3',
     packages=setuptools.find_packages(),
     description='Lots of metrics for quantifying gerrymandering',
     author='Princeton Gerrymandering Project',
